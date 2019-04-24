e88649b91c5a75d92b1c57291be813f5e5294753
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2019-04-24 12:54:38 +0100

    fix redux dev tools error

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,16 @@
 import thunk from 'redux-thunk';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 
 
 export default () => {
   const store = createStore(
     combineReducers({
     }),
-    applyMiddleware(thunk)
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 
   return store;
